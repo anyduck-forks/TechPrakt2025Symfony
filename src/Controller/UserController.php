6cfded5d0aad1e5d2a9a0e81b65126244bbaeb2c
Dmytro Herashchenko <51529317+anyduck@users.noreply.github.com>
2025-02-07 20:10:58 +0200

    feat: add endpoint for user data by id

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,36 +9,44 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class UserController
 {
+    private const TEST_USERS = [
+        [
+            'id'    => '1',
+            'email' => 'ipz231_gdi1@student.ztu.edu.ua',
+        ],
+        [
+            'id'    => '2',
+            'email' => 'ipz231_gdi2@student.ztu.edu.ua',
+        ],
+        [
+            'id'    => '3',
+            'email' => 'ipz231_gdi3@student.ztu.edu.ua',
+        ],
+        [
+            'id'    => '4',
+            'email' => 'ipz231_gdi4@student.ztu.edu.ua',
+        ],
+        [
+            'id'    => '5',
+            'email' => 'ipz231_gdi5@student.ztu.edu.ua',
+        ],
+    ];
+
     #[Route('/users', name: 'app_collection_users', methods: [Request::METHOD_GET])]
     public function get_collection(): JsonResponse
     {
         return new JsonResponse([
-            'data' => [
-                [
-                    'id'    => '1',
-                    'email' => 'ipz231_gdi1@student.ztu.edu.ua',
-                ],
-                [
-                    'id'    => '2',
-                    'email' => 'ipz231_gdi2@student.ztu.edu.ua',
-                ],
-                [
-                    'id'    => '3',
-                    'email' => 'ipz231_gdi3@student.ztu.edu.ua',
-                ],
-                [
-                    'id'    => '3',
-                    'email' => 'ipz231_gdi3@student.ztu.edu.ua',
-                ],
-                [
-                    'id'    => '4',
-                    'email' => 'ipz231_gdi4@student.ztu.edu.ua',
-                ],
-                [
-                    'id'    => '5',
-                    'email' => 'ipz231_gdi5@student.ztu.edu.ua',
-                ],
-            ],
+            'data' => self::TEST_USERS,
+        ], Response::HTTP_OK);
+    }
+
+    #[Route('/users/{id}', name: 'app_item_users', methods: [Request::METHOD_GET])]
+    public function get_item(string $id): JsonResponse
+    {
+        $user = current(array_filter(self::TEST_USERS, fn($user) => $user['id'] === $id));
+
+        return new JsonResponse([
+            'data' => $user
         ], Response::HTTP_OK);
     }
 }
